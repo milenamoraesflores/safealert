ad67dbf3c32bf92bfe0404a6f4d1ec093fd292a1
Milena Flores <119636682+milenamoraesflores@users.noreply.github.com>
2025-05-19 23:10:45 -0300

    chore: add link youtube video

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -95,7 +95,7 @@ const About: React.FC = () => {
           <iframe
             width="100%"
             height="450"
-            src="https://www.youtube.com/embed/SEU_VIDEO_ID"
+            src="https://youtu.be/0fSZzLJX9wI?si=FTOOOtuMnWFtXU4J"
             title="Pitch do Sistema de Previsão de Deslizamentos"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
